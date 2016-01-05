338207591d2b98d604ee9c405e5a448211ad5fb4
Ashley Mannix <ashleymannix@live.com.au>
2016-01-05 19:14:01 +1000

    indentation

--- codegen/src/api/gen/mod.rs
+++ codegen/src/api/gen/mod.rs
@@ -12,51 +12,51 @@ impl RustApiFnGen {
 		//function
 		let mut fun = FnDecl {
 			inputs: vec![
-				//arg1: &mut i32
-		        Arg {
-		        	//&mut i32
-		            ty: P(Ty {
-		                id: DUMMY_NODE_ID,
-		                //&mut
-		                node: Ty_::TyRptr(
-		                	None, 
-		                	MutTy { 
-		                		//i32
-		                		ty: P(Ty {
-					                id: DUMMY_NODE_ID,
-					                node: Ty_::TyPath(None, Path {
-					                	span: DUMMY_SP,
-					                	global: false,
-					                	segments: vec![ 
-					                		PathSegment { 
-					                			identifier: token::str_to_ident("i32"), 
-					                			parameters: PathParameters::none()
-					                		} 
-					                	]
-					                }),
-					                span: DUMMY_SP,
-					            }),
-					            //mut
-		                		mutbl: Mutability::MutMutable
-		                	}
-		                ),
-		                span: DUMMY_SP,
-		            }),
-		            //arg1
-		            pat: P(Pat {
-		                id: DUMMY_NODE_ID,
-		                node: PatIdent(
-		                	BindingMode::ByValue(Mutability::MutImmutable), 
-		                	Spanned { 
-		                		span: DUMMY_SP, 
-		                		node: token::str_to_ident("arg1") 
-		                	}, 
-		                	None
-		                ),
-		                span: DUMMY_SP
-		            }),
-		            id: DUMMY_NODE_ID
-		        }
+			//arg1: &mut i32
+			Arg {
+				//&mut i32
+				ty: P(Ty {
+					id: DUMMY_NODE_ID,
+					//&mut
+					node: Ty_::TyRptr(
+						None, 
+						MutTy { 
+							//i32
+							ty: P(Ty {
+								id: DUMMY_NODE_ID,
+								node: Ty_::TyPath(None, Path {
+									span: DUMMY_SP,
+									global: false,
+									segments: vec![ 
+									PathSegment { 
+										identifier: token::str_to_ident("i32"), 
+										parameters: PathParameters::none()
+									} 
+									]
+								}),
+								span: DUMMY_SP,
+							}),
+							//mut
+							mutbl: Mutability::MutMutable
+						}
+						),
+					span: DUMMY_SP,
+				}),
+				//arg1
+				pat: P(Pat {
+					id: DUMMY_NODE_ID,
+					node: PatIdent(
+						BindingMode::ByValue(Mutability::MutImmutable), 
+						Spanned { 
+							span: DUMMY_SP, 
+							node: token::str_to_ident("arg1") 
+						}, 
+						None
+						),
+					span: DUMMY_SP
+				}),
+				id: DUMMY_NODE_ID
+			}
 			],
 			//No return
 			output: FunctionRetTy::DefaultReturn(DUMMY_SP),
