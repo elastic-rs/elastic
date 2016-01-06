32f9251dedb3b2cd1daf7358251b0346a4a79c47
Ashley Mannix <ashleymannix@live.com.au>
2016-01-06 16:28:54 +1000

    more work on codegen

--- codegen/src/api/gen/mod.rs
+++ codegen/src/api/gen/mod.rs
@@ -1,107 +1,181 @@
 use std::intrinsics::type_name;
-use syntax::ast;
-use syntax::ast::*;
+use syntax::ast::{ 
+	Ident, 
+	FnDecl, 
+	Generics, 
+	Block, 
+	BlockCheckMode,
+	Unsafety, 
+	Constness, 
+	FunctionRetTy, 
+	Pat, 
+	PatIdent, 
+	BindingMode, 
+	Ty, 
+	Ty_, 
+	MutTy, 
+	Arg,
+	Path,
+	PathParameters, 
+	PathSegment, 
+	DUMMY_NODE_ID 
+};
 use syntax::parse::token;
-use syntax::codemap::{ Span, Spanned, DUMMY_SP };
+use syntax::print::pprust;
+use syntax::codemap::{ Spanned, DUMMY_SP };
 use syntax::ptr::P;
 
+/// Rexport of the libsyntax Mutability enum
+pub use syntax::ast::Mutability;
+
+/// A representation of a Rust fn
 pub struct Fn {
+	/// The name of the fn
 	pub identifier: Ident,
+	/// The fn header with arguments and return type
 	pub decl: FnDecl,
+	/// The lifetimes and generic params for the fn
 	pub generics: Generics,
+	/// The body of the fn
+	pub body: Block,
+	/// Whether or not the fn is unsafe
 	pub unsafety: Unsafety,
+	/// Whether or not the fn is constant
 	pub constness: Constness
 }
 
-/// Generates function signatures for the Elasticsearch API
-pub struct RustApiFnGen;
-impl RustApiFnGen {
-	//TODO: Fill in this method properly
-	pub fn gen_fn(name: &str) -> Fn {
-		//function
-		let mut fun = FnDecl {
-			inputs: vec![
-			//arg1: &mut i32
-			Arg {
-				//&mut i32
-				ty: P(RustApiFnGen::gen_ty_ptr::<i32>(Mutability::MutMutable)),
-				//arg1
-				pat: P(Pat {
-					id: DUMMY_NODE_ID,
-					node: PatIdent(
-						BindingMode::ByValue(Mutability::MutImmutable), 
-						Spanned { 
-							span: DUMMY_SP, 
-							node: token::str_to_ident("arg1") 
-						}, 
-						None
-						),
-					span: DUMMY_SP
-				}),
-				id: DUMMY_NODE_ID
-			}
-			],
-			//No return
-			output: FunctionRetTy::DefaultReturn(DUMMY_SP),
-			variadic: false
-		};
+impl ToString for Fn {
+	/// Outputs the fn declaration and body as Rust source
+	fn to_string(&self) -> String {
+		let decl = pprust::fun_to_string(
+			&self.decl, 
+			self.unsafety, 
+			self.constness, 
+			self.identifier, 
+			None, 
+			&self.generics
+		);
 
+		let body = pprust::block_to_string(&self.body);
+
+		decl + &body
+	}
+}
+
+/// Generates function signatures for the Elasticsearch API
+pub struct RustApiFnBldr;
+impl RustApiFnBldr {
+	/// Create a new builder
+	pub fn new() -> RustApiFnBldr {
+		RustApiFnBldr
+	}
+
+	/// Generate a function
+	pub fn gen_fn(&self, name: &str, inputs: Vec<Arg>) -> Fn {
 		Fn {
 			identifier: token::str_to_ident(name),
-			decl: fun, 
+			decl: FnDecl {
+				inputs: inputs,
+				output: FunctionRetTy::DefaultReturn(DUMMY_SP),
+				variadic: false
+			},
 			generics: Generics::default(),
-			unsafety: Unsafety::Normal, 
+			body: Block {
+				stmts: Vec::new(),
+				expr: None,
+				id: DUMMY_NODE_ID,
+				rules: BlockCheckMode::DefaultBlock,
+				span: DUMMY_SP
+			},
+			unsafety: Unsafety::Normal,
 			constness: Constness::NotConst
 		}
 	}
 
-	pub fn gen_ty<T>() -> Ty {
+	/// Generate a function arg with a type
+	pub fn gen_arg(&self, name: &str, ty: Ty) -> Arg {
+		Arg {
+			ty: P(ty),
+			pat: P(Pat {
+				id: DUMMY_NODE_ID,
+				node: PatIdent(
+					BindingMode::ByValue(Mutability::MutImmutable),
+					Spanned {
+						span: DUMMY_SP,
+						node: token::str_to_ident(name)
+					},
+					None
+					),
+				span: DUMMY_SP
+			}),
+			id: DUMMY_NODE_ID
+		}
+	}
+
+	/// Generate a type with a specified name
+	pub fn gen_ty_as(&self, name: &str) -> Ty {
 		Ty {
 			id: DUMMY_NODE_ID,
 			node: Ty_::TyPath(None, Path {
 				span: DUMMY_SP,
 				global: false,
-				segments: vec![ 
-				PathSegment { 
-					identifier: token::str_to_ident(name_of::<T>()), 
+				segments: vec![
+				PathSegment {
+					identifier: token::str_to_ident(name),
 					parameters: PathParameters::none()
 				}]
 			}),
-			span: DUMMY_SP,
+			span: DUMMY_SP
 		}
 	}
 
-	pub fn gen_ty_from<T>(_: &T) -> Ty {
-		RustApiFnGen::gen_ty::<T>()
+	/// Generate a type
+	pub fn gen_ty<T>(&self) -> Ty {
+		self.gen_ty_as(name_of::<T>())
 	}
 
-	pub fn gen_ty_mut<T>(mutbl: Mutability) -> MutTy {
+	/// Generate a potentially mutable type with a specified name
+	pub fn gen_ty_mut_as(&self, name: &str, mutbl: Mutability) -> MutTy {
 		MutTy {
-			ty: P(RustApiFnGen::gen_ty::<T>()),
+			ty: P(self.gen_ty_as(name)),
 			mutbl: mutbl
 		}
 	}
 
-	pub fn gen_ty_mut_from<T>(mutbl: Mutability, _: &T) -> MutTy {
-		RustApiFnGen::gen_ty_mut::<T>(mutbl)
+	/// Generate a potentially mutable type
+	pub fn gen_ty_mut<T>(&self, mutbl: Mutability) -> MutTy {
+		MutTy {
+			ty: P(self.gen_ty::<T>()),
+			mutbl: mutbl
+		}
 	}
 
-	pub fn gen_ty_ptr<T>(mutbl: Mutability) -> Ty {
+	/// Generate a type pointer with a specified name
+	pub fn gen_ty_ptr_as<T>(&self, name: &str, mutbl: Mutability) -> Ty {
 		Ty {
 			id: DUMMY_NODE_ID,
 			node: Ty_::TyRptr(
-				None, 
-				RustApiFnGen::gen_ty_mut::<T>(mutbl)
+				None,
+				self.gen_ty_mut_as(name, mutbl)
 			),
 			span: DUMMY_SP
 		}
 	}
 
-	pub fn gen_ty_ptr_from<T>(mutbl: Mutability, _: &T) -> Ty {
-		RustApiFnGen::gen_ty_ptr::<T>(mutbl)
+	/// Generate a type pointer
+	pub fn gen_ty_ptr<T>(&self, mutbl: Mutability) -> Ty {
+		Ty {
+			id: DUMMY_NODE_ID,
+			node: Ty_::TyRptr(
+				None,
+				self.gen_ty_mut::<T>(mutbl)
+			),
+			span: DUMMY_SP
+		}
 	}
 }
 
+/// Get the full-path name of a type
 pub fn name_of<'a, T>() -> &'a str {
     let t =
         unsafe {
@@ -110,6 +184,7 @@ pub fn name_of<'a, T>() -> &'a str {
     t
 }
 
+/// Get the full-path name of a type inferred from the argument
 pub fn type_of<T>(_: &T) -> &str {
     name_of::<T>()
 }
--- codegen/tests/gen.rs
+++ codegen/tests/gen.rs
@@ -3,20 +3,22 @@
 extern crate elastic_codegen;
 extern crate syntax;
 
-use elastic_codegen::api::gen::RustApiFnGen;
+use elastic_codegen::api::gen::*;
 
 #[test]
 fn can_build_rust_fn_from_ast() {
-	let fun = RustApiFnGen::gen_fn("my_fun");
-	let fun_str = syntax::print::pprust::fun_to_string(
-		&fun.decl, 
-		fun.unsafety, 
-		fun.constness, 
-		fun.identifier, 
-		None, 
-		&fun.generics
-	);
+	//Create a builder
+	let bldr = RustApiFnBldr::new();
 
-	println!("{}", fun_str);
-	panic!("implement");
+	//Get a function signature
+	let fun = bldr.gen_fn("my_fun", vec![
+		bldr.gen_arg("arg1", bldr.gen_ty_ptr::<i32>(Mutability::MutMutable)),
+		bldr.gen_arg("arg2", bldr.gen_ty_as("String"))
+	]);
+
+	//Write the function
+	let fun_str = fun.to_string();
+	
+	//TODO: This is a crummy way to test a crummy ast builder. But is ok to just verify things are moving in the right direction
+	assert_eq!("fn my_fun(arg1: &mut i32, arg2: String){ }", &fun_str);
 }
