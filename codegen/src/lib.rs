ca2d802e0bb7f3d4ec37eaf47e06e62171e3bbb9
Ashley Mannix <admin@kodr.com.au>
2016-01-12 13:42:46 +1000

    add doc to codegen crate

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,3 +1,20 @@
+//! Elasticsearch Codegen
+//!
+//! A library that contains useful structures and functions for generating code from the Elasticsearch API specification.
+//! 
+//! The library is organised into a few layers:
+//! 
+//! - Parsing API / Test source to an AST
+//! - Getting URLs and their required parameters for API endpoints
+//! - Helpers for Rust codegen from an API AST
+//! 
+//! A consumer of this library can take advantage of any layer and those below it for their desired level of abstraction.
+//! For example, currently only Rust codegen helpers are included through the `libsyntax` crate, but other languages could be added on top of the same API AST.
+//! 
+//! # Links
+//! - [Spec Source](https://github.com/elastic/elasticsearch/tree/master/rest-api-spec)
+//! - [Github](https://github.com/KodrAus/elasticsearch-rs)
+
 //TODO: Uncomment attr
 //#![deny(missing_docs)]
 
