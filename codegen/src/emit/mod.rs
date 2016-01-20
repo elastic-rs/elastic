adbf265133a7c6b4364707ba2c0da91ef71cb480
Ashley Mannix <ashleymannix@live.com.au>
2016-01-20 19:06:54 +1000

    adding a context free emitter

--- codegen/src/emit/mod.rs
+++ codegen/src/emit/mod.rs
@@ -4,6 +4,7 @@
 
 pub mod rust;
 
+use std::marker::PhantomData;
 use std::io::Write;
 use std::error;
 use std::fmt;
@@ -39,27 +40,93 @@ pub trait Emit<T, E> where E: Into<EmitError> {
 pub trait Emitter<'a> {
 	/// A context struct that's threaded through calls to `Emit::emit`.
 	type Ctxt: 'a;
+	/// The context passed to the implementors of `Emit`.
+	type CtxtBrw: 'a = &'a Self::Ctxt;
+
 	/// An error returned by `emit()`.
 	type Error: From<EmitError> = EmitError;
 	
 	/// Gets the context struct.
-	fn get_cx(&self) -> &'a Self::Ctxt;
+	fn get_cx(&self) -> Self::CtxtBrw;
 	
 	/// Emit a codegen item to the provided writer.
 	/// 
 	/// This default implementation will attempt to emit results in-line, 
 	/// so no extra characters, such as new lines or whitespace, will be emitted.
-	fn emit<Em, Er, W>(&self, e: &'a Em, writer: &'a mut W) -> Result<(), Self::Error> 
-	where Em: Emit<&'a Self::Ctxt, Er>, Er: Into<EmitError>, W: Write {
-		let emitted = try!(
-			e.emit(self.get_cx())
-				.map_err(|e| e.into())
-		);
-		
-		writer.write_all(&emitted.into_bytes()[..]).map_err(|e| {
-			let emiterr: EmitError = e.into();
-			emiterr.into()
-		})
+	fn emit<Em, Er, W>(&self, e: &'a Em, writer: &'a mut W) -> Result<(), Self::Error> where 
+		Em: Emit<Self::CtxtBrw, Er>, 
+		Er: Into<EmitError>, 
+		W: Write {
+			DefaultEmitter::emit::<Self::CtxtBrw, Self::Error, Em, Er, W>(self.get_cx(), e, writer)
+	}
+}
+
+/// Default context-free Emitter
+/// 
+/// This emitter will emit items that implement `Emit<(), _>`
+pub struct CtxtFreeEmitter<E = EmitError> where E: From<EmitError> {
+	phantom: PhantomData<E>
+}
+
+impl <E> CtxtFreeEmitter<E> where E: From<EmitError> {
+	/// Creates a new emitter.
+	/// 
+	/// # Examples
+	/// 
+	/// Create a default `CtxtFreeEmitter`:
+	/// 
+	/// ```
+	/// use elastic_codegen::emit::*;
+	/// 
+	/// let emitter: CtxtFreeEmitter = CtxtFreeEmitter::new();
+	/// ```
+	/// 
+	/// Create a `CtxtFreeEmitter` with the specified `Error` type:
+	/// 
+	/// ```
+	/// use elastic_codegen::emit::*;
+	/// 
+	/// let emitter = CtxtFreeEmitter::<EmitError>::new();
+	/// ```
+	pub fn new() -> CtxtFreeEmitter<E> {
+		CtxtFreeEmitter::<E> {
+			phantom: PhantomData
+		}
+	}
+}
+
+impl <'a, E> Emitter<'a> for CtxtFreeEmitter<E> where E: From<EmitError> {
+	type Ctxt = ();
+	type CtxtBrw = ();
+	type Error = E;
+
+	fn get_cx(&self) {
+
+	}
+
+	fn emit<Em, Er, W>(&self, e: &'a Em, writer: &'a mut W) -> Result<(), Self::Error> where 
+		Em: Emit<Self::CtxtBrw, Er>, 
+		Er: Into<EmitError>, 
+		W: Write {
+			DefaultEmitter::emit::<Self::CtxtBrw, Self::Error, Em, Er, W>(self.get_cx(), e, writer)
+	}
+}
+
+struct DefaultEmitter;
+impl DefaultEmitter {
+	pub fn emit<'a, Cb, E, Em, Er, W>(c: Cb, e: &'a Em, writer: &'a mut W) -> Result<(), E>  where 
+		Em: Emit<Cb, Er>, 
+		E: From<EmitError>, 
+		Er: Into<EmitError>, 
+		W: Write {
+			let emitted = try!(
+				e.emit(c).map_err(|e| e.into())
+			);
+			
+			writer.write_all(&emitted.into_bytes()[..]).map_err(|e| {
+				let emit_err: EmitError = e.into();
+				emit_err.into()
+			})
 	}
 }
 
