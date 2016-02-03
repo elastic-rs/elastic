2c075e15072c834f275366e0d7970431a14224dd
Ashley Mannix <admin@kodr.com.au>
2016-02-03 12:24:47 +1000

    Update mod.rs

--- types/src/date/mod.rs
+++ types/src/date/mod.rs
@@ -1,7 +1,9 @@
 //! Implementation of the Elasticsearch `date` type.
 //! 
 //! Dates in Elasticsearch are exposed as a formatted `string` which can contain a `date` and/or a `time` component.
+//! 
 //! All dates are expected to be given in `UTC`, and if no time is supplied, then 12:00am will be used instead.
+//! Where performance is paramount, the `EpochMillis` date format will parse and format dates the fastest.
 //! 
 //! # Links
 //! - [Elasticsearch Doc](https://www.elastic.co/guide/en/elasticsearch/reference/current/date.html)
