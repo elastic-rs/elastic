fe80864ea459d3b70f927b51ba96a9190ed5430e
Ashley Mannix <Ashley.Mannix@sunshinecoast.qld.gov.au>
2016-02-12 11:35:46 +1000

    tweaks to csharp bench

--- dev/es_benches_csharp/Program.cs
+++ dev/es_benches_csharp/Program.cs
@@ -9,34 +9,50 @@ namespace ElasticsearchNetTest
         static void Main(string[] args)
         {
             //ElasticsearchNet
-            var netwatch = Stopwatch.StartNew();
-                var netclient = new Elasticsearch.Net.ElasticLowLevelClient();
-                netclient.Search<Stream>("bench_index", "docs", new Elasticsearch.Net.PostData<object>(
-                    @"{
-                      'query': {
-                        'query_string': {
-                          'default_field': 'title',
-                          'query': 'doc'
+            double total = 0;
+            int c = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                var netwatch = Stopwatch.StartNew();
+                    var netclient = new Elasticsearch.Net.ElasticLowLevelClient();
+                    netclient.Search<Stream>("bench_index", "docs", new Elasticsearch.Net.PostData<object>(
+                        @"{
+                        'query': {
+                            'query_string': {
+                            'default_field': 'title',
+                            'query': 'doc'
+                            }
                         }
-                      }
-                    }"
-                ));
-            netwatch.Stop();
-            Console.WriteLine("Elasticsearch.NET: {0}ns", (netwatch.Elapsed.TotalMilliseconds * 1000000).ToString("n0"));
+                        }"
+                    ));
+                netwatch.Stop();
+                total += netwatch.Elapsed.TotalMilliseconds;
+                c++;
+            }
+            Console.WriteLine("Elasticsearch.NET: {0}ns", ((total / c) * 1000000).ToString("n0"));
 
             //NEST
-            var nestwatch = Stopwatch.StartNew();
-                var nestclient = new Nest.ElasticClient();
-                nestclient.Search<object>(body => body
-                    .Query(query => query
-                        .QueryString(qrystr => qrystr
-                            .Query("doc")
-                            .DefaultField("title")
+            total = 0;
+            c = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                var nestwatch = Stopwatch.StartNew();
+                    var nestclient = new Nest.ElasticClient();
+                    nestclient.Search<object>(body => body
+                        .Index("bench_index")
+                        .Type("docs")
+                        .Query(query => query
+                            .QueryString(qrystr => qrystr
+                                .Query("doc")
+                                .DefaultField("title")
+                            )
                         )
-                    )
-                );
-            nestwatch.Stop();
-            Console.WriteLine("NEST: {0}ns", (nestwatch.Elapsed.TotalMilliseconds * 1000000).ToString("n0"));
+                    );
+                nestwatch.Stop();
+                total += nestwatch.Elapsed.TotalMilliseconds;
+                c++;
+            }
+            Console.WriteLine("NEST: {0}ns", ((total / c) * 1000000).ToString("n0"));
         }
     }
 }
