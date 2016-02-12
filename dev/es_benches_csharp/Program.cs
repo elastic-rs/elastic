43a7e579301b41e934debe8069449b15b1653026
Ashley Mannix <Ashley.Mannix@sunshinecoast.qld.gov.au>
2016-02-12 11:26:29 +1000

    unscientific benches for csharp client

---
+++ dev/es_benches_csharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ElasticsearchNetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ElasticsearchNet
+            var netwatch = Stopwatch.StartNew();
+                var netclient = new Elasticsearch.Net.ElasticLowLevelClient();
+                netclient.Search<Stream>("bench_index", "docs", new Elasticsearch.Net.PostData<object>(
+                    @"{
+                      'query': {
+                        'query_string': {
+                          'default_field': 'title',
+                          'query': 'doc'
+                        }
+                      }
+                    }"
+                ));
+            netwatch.Stop();
+            Console.WriteLine("Elasticsearch.NET: {0}ns", (netwatch.Elapsed.TotalMilliseconds * 1000000).ToString("n0"));
+
+            //NEST
+            var nestwatch = Stopwatch.StartNew();
+                var nestclient = new Nest.ElasticClient();
+                nestclient.Search<object>(body => body
+                    .Query(query => query
+                        .QueryString(qrystr => qrystr
+                            .Query("doc")
+                            .DefaultField("title")
+                        )
+                    )
+                );
+            nestwatch.Stop();
+            Console.WriteLine("NEST: {0}ns", (nestwatch.Elapsed.TotalMilliseconds * 1000000).ToString("n0"));
+        }
+    }
+}
