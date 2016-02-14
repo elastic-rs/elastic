6a6caeb33092afceb5cfb66abd8502d043cfebd2
Ashley Mannix <ashleymannix@live.com.au>
2016-02-14 17:04:03 +1000

    adding initial rotor bench

--- dev/es_benches_rust/benches/mod.rs
+++ dev/es_benches_rust/benches/mod.rs
@@ -16,7 +16,11 @@ Working on a few ideas for the client implementation.
 
 This is using rs-es as a benchmark.
 
+There's absolutely nothing scientific going on here, and only synchronous requests are being benchmarked,
+even though an asynchronous API is a primary goal. Watch this space :)
+
 */
 
 pub mod hyper;
+pub mod rotor;
 pub mod rses;
--- dev/es_benches_rust/src/main.rs
+++ dev/es_benches_rust/src/main.rs
@@ -1,3 +1,7 @@
+//! This is just a simple crate for mocking out simple client implementations
+//! 
+//! See the benches for progress.
+
 #![feature(plugin)]
 #![plugin(elastic_types_codegen)]
 #![plugin(serde_macros)]
@@ -6,6 +10,9 @@ extern crate stopwatch;
 extern crate serde;
 extern crate serde_json;
 extern crate hyper;
+extern crate rotor;
+extern crate rotor_http;
+
 extern crate elastic_types_codegen;
 
 fn main() {
