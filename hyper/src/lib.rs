ac709e2d21dde23d681485ab5f45d1adb21b2a8f
Ashley Mannix <ashleymannix@live.com.au>
2016-02-28 09:38:45 +1000

    running rustfmt over genned code

--- hyper/src/lib.rs
+++ hyper/src/lib.rs
@@ -1,10 +1,12 @@
 //! Elasticsearch Hyper Client
 //!
 //! A lightweight implementation of the Elasticsearch API based on Hyper.
-//! 
-//! Each API endpoint is represented as its own function, so each possible http route gets its own function.
-//! The functions are also designed to work well with the `elastic_types` and `elastic_types_codegen` crates, but deserialisation is the responsibility of the caller.
-//! 
+//!
+//! Each API endpoint is represented as its own function,
+//! so each possible http route gets its own function.
+//! The functions are also designed to work well with the `elastic_types`
+//! and `elastic_types_codegen` crates, but deserialisation is the responsibility of the caller.
+//!
 //! # Links
 //! - [elastic_types](http://kodraus.github.io/rustdoc/elastic_types/index.html)
 //! - [elastic_types_codegen](http://kodraus.github.io/rustdoc/elastic_types_codegen/index.html)
