dcfd8208f1af1967ebcc9a026f6dbdca93018a30
Ashley Mannix <ashleymannix@live.com.au>
2016-04-18 19:57:08 +1000

    updating bool docs

--- types/src/boolean/mod.rs
+++ types/src/boolean/mod.rs
@@ -1,4 +1,33 @@
 //! Implementation of the Elasticsearch `boolean` types.
+//!
+//! # Examples
+//!
+//! Map with a default `boolean`:
+//!
+//! ```
+//! struct MyType {
+//! 	pub field: bool
+//! }
+//! ```
+//!
+//! Map with a custom `boolean`:
+//!
+//! ```
+//! # extern crate serde;
+//! # #[macro_use]
+//! # extern crate elastic_types;
+//! # fn main() {
+//! # use elastic_types::mapping::prelude::*;
+//! # use elastic_types::boolean::prelude::*;
+//! # #[derive(Debug, Clone, Default)]
+//! # pub struct MyBooleanMapping;
+//! # impl ElasticBooleanMapping for MyBooleanMapping { }
+//! # impl_boolean_mapping!(MyBooleanMapping);
+//! struct MyType {
+//! 	pub field: ElasticBoolean<MyBooleanMapping>
+//! }
+//! # }
+//! ```
 
 mod boolean;
 
