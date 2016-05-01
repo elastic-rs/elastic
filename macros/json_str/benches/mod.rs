1cbfd66655a193676720e72af2ea349dd17b2c3f
Ashley Mannix <ashleymannix@live.com.au>
2016-05-01 12:04:20 +1000

    fix json_str benches

--- macros/json_str/benches/mod.rs
+++ macros/json_str/benches/mod.rs
@@ -1,11 +1,7 @@
 #![feature(test, plugin)]
 #![plugin(json_str)]
-#![plugin(serde_macros)]
-#![plugin(json_macros)]
 
 extern crate test;
-extern crate serde;
-extern crate serde_json;
 
 use test::Bencher;
 
@@ -117,116 +113,5 @@ fn parse_plain_json_str_lrg(b: &mut Bencher) {
 				}
 			}
 		})
-	});
-}
-
-#[bench]
-fn parse_plain_json_value_sml(b: &mut Bencher) {
-	b.iter(|| {
-		serde_json::to_string(&json!({
-			"query": {
-				"filtered": {
-					"query": {
-						"match_all": {}
-					},
-					"filter": {
-						"geo_distance": {
-							"distance": "20km",
-							"location": {
-								"lat": 37.776,
-								"lon": -122.41
-							}
-						}
-					}
-				}
-			}
-		}))
-	});
-}
-
-#[bench]
-fn parse_plain_json_value_med(b: &mut Bencher) {
-	b.iter(|| {
-		serde_json::to_string(&json!({
-			"query": {
-				"filtered": {
-					"query": {
-						"filtered": {
-							"query": {
-								"match_all": {}
-							},
-							"filter": {
-								"geo_distance": {
-									"distance": "20km",
-									"location": {
-										"lat": 37.776,
-										"lon": -122.41
-									}
-								}
-							}
-						}
-					},
-					"filter": {
-						"geo_distance": {
-							"distance": "20km",
-							"location": {
-								"lat": 37.776,
-								"lon": -122.41
-							}
-						}
-					}
-				}
-			}
-		}))
-	});
-}
-
-#[bench]
-fn parse_plain_json_value_lrg(b: &mut Bencher) {
-	b.iter(|| {
-		serde_json::to_string(&json!({
-			"query": {
-				"filtered": {
-					"query": {
-						"filtered": {
-							"query": {
-								"filtered": {
-									"query": {
-										"match_all": {}
-									},
-									"filter": {
-										"geo_distance": {
-											"distance": "20km",
-											"location": {
-												"lat": 37.776,
-												"lon": -122.41
-											}
-										}
-									}
-								}
-							},
-							"filter": {
-								"geo_distance": {
-									"distance": "20km",
-									"location": {
-										"lat": 37.776,
-										"lon": -122.41
-									}
-								}
-							}
-						}
-					},
-					"filter": {
-						"geo_distance": {
-							"distance": "20km",
-							"location": {
-								"lat": 37.776,
-								"lon": -122.41
-							}
-						}
-					}
-				}
-			}
-		}))
 	});
 }
