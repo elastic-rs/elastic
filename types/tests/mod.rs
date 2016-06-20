2c438879eba2ee462524d64493ef027fdd63f205
Ashley Mannix <ashleymannix@live.com.au>
2016-06-20 13:59:15 +1000

    add map tests

--- types/tests/mod.rs
+++ types/tests/mod.rs
@@ -538,6 +538,7 @@ pub mod geo_shape_fixtures {
 }
 
 pub mod object_fixtures {
+	use std::collections::BTreeMap;
 	use std::net::Ipv4Addr;
 	use chrono::{ DateTime, UTC };
 	use elastic_types::mapping::prelude::*;
@@ -566,7 +567,9 @@ pub mod object_fixtures {
 		pub my_bool1: bool,
 		pub my_bool2: ElasticBoolean<MyBooleanMapping>,
 		pub my_geo: Option<ElasticGeoShape<MyGeoShapeMapping>>,
-		pub my_ips: Option<Vec<Ipv4Addr>>
+		pub my_ips: Option<Vec<Ipv4Addr>>,
+		pub my_map1: BTreeMap<String, String>,
+		pub my_map2: serde_json::Value
 	}
 
 	#[derive(Default, Clone)]
