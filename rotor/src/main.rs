598e55a57e1749f24859a5add041d6b82aa56bdd
Ashley Mannix <ashleymannix@live.com.au>
2016-06-27 16:50:55 +1000

    adding notes to rotor concept

--- rotor/src/main.rs
+++ rotor/src/main.rs
@@ -1,7 +1,14 @@
+//rotor_http scratchpad
+
+//Expects Elasticsearch cluster at http://localhost:9200
+//The loop won't close on its own, you have to kill the process yourself
+
 //Adapted from: https://github.com/tailhook/rotor-http/blob/master/examples/client.rs
 //TODO: Mess with this to build a generic Elasticsearch rotor impl
+//Need to figure out what the best way to implement certain features is.
 
 extern crate rotor;
+extern crate rotor_tools;
 extern crate rotor_http;
 extern crate url;
 
@@ -10,12 +17,14 @@ use std::io::Write;
 use std::net::ToSocketAddrs;
 use std::time::Duration;
 use std::process::exit;
+use std::thread;
+use std::sync::mpsc;
 
 use url::Url;
-use rotor::{Scope, Time};
-use rotor_http::client::{connect_tcp, Request, Head, Client, RecvMode};
-use rotor_http::client::{Connection, Requester, Task, Version, ResponseError};
-use rotor_http::client::{ProtocolError};
+use rotor::{ Scope, Time };
+use rotor_tools::loop_ext::LoopExt;
+use rotor_http::client::{ connect_tcp, Request, Head, Client, RecvMode };
+use rotor_http::client::{ Connection, Requester, Task, Version, ResponseError, ProtocolError };
 
 struct Context;
 
@@ -29,16 +38,25 @@ impl Client for Cli {
         _scope: &mut Scope<<Self::Requester as Requester>::Context>)
         -> Self
     {
+    	println!("Cli.create");
+
         Cli(Some(seed))
     }
     fn connection_idle(mut self, _conn: &Connection,
         scope: &mut Scope<Context>)
         -> Task<Cli>
     {
+    	//TODO: Pull req url off queue.
+    	//Avoid spinning, if nothing is there, wait for a wakeup?
+
+    	println!("Cli.connection_idle");
+
         match self.0.take() {
-            Some(url) => Task::Request(Cli(None), Req(url)),
+            Some(url) => {
+            	Task::Request(Cli(None), Req(url))
+            },
             None => {
-                scope.shutdown_loop();
+                //scope.shutdown_loop();
                 Task::Close
             }
         }
@@ -46,6 +64,8 @@ impl Client for Cli {
     fn connection_error(self, err: &ProtocolError,
         _scope: &mut Scope<Context>)
     {
+    	println!("Cli.connection_error");
+
         writeln!(&mut stderr(), "----- Bad response: {} -----", err).ok();
         exit(1);
     }
@@ -54,6 +74,8 @@ impl Client for Cli {
         _scope: &mut Scope<<Self::Requester as Requester>::Context>)
         -> Task<Cli>
     {
+    	println!("Cli.wakeup");
+
         unimplemented!();
     }
     fn timeout(self,
@@ -71,8 +93,11 @@ impl Requester for Req {
         _scope: &mut Scope<Self::Context>)
         -> Option<Self>
     {
+    	println!("Req.prepare_request");
+
         req.start("GET", &self.0.path(), Version::Http11);
         req.add_header("Host", self.0.host_str().unwrap().as_bytes()).unwrap();
+        req.add_header("Connection", b"keep-alive").unwrap();
         req.done_headers().unwrap();
         req.done();
         Some(self)
@@ -81,6 +106,8 @@ impl Requester for Req {
         scope: &mut Scope<Self::Context>)
         -> Option<(Self, RecvMode, Time)>
     {
+    	println!("Req.headers_received");
+
         println!("----- Headers -----");
         println!("Status: {} {}", head.code, head.reason);
         for header in head.headers {
@@ -93,6 +120,8 @@ impl Requester for Req {
     fn response_received(self, data: &[u8], _request: &mut Request,
         _scope: &mut Scope<Self::Context>)
     {
+    	println!("Req.response_received");
+
         println!("----- Response -----");
         stdout().write_all(data).unwrap();
         if data.last() != Some(&b'\n') {
@@ -122,20 +151,30 @@ impl Requester for Req {
     }
     fn bad_response(self, err: &ResponseError, _scope: &mut Scope<Context>)
     {
+    	println!("Req.bad_response");
+
         writeln!(&mut stderr(), "----- Bad response: {} -----", err).ok();
         exit(1);
     }
 }
 
 fn main() {
-    let url = Url::parse("http://info.cern.ch/hypertext/WWW/TheProject.html").unwrap();
+	let url = Url::parse("http://localhost:9200").unwrap();
     let addr = url.to_socket_addrs().unwrap().next().unwrap();
 
-    let creator = rotor::Loop::new(&rotor::Config::new()).unwrap();
-    let mut loop_inst = creator.instantiate(Context);
-    loop_inst.add_machine_with(|scope| {
-        connect_tcp::<Cli>(scope, &addr, url)
-    }).unwrap();
+    //TODO: Add notifier machine for registering new connections?
+    //Use https://github.com/aturon/crossbeam/blob/master/src/sync/ms_queue.rs
 
-    loop_inst.run().unwrap();
+	let handle = thread::spawn(move || {
+	    let creator = rotor::Loop::new(&rotor::Config::new()).unwrap();
+    	let mut loop_inst = creator.instantiate(Context);
+
+	    loop_inst.add_machine_with(|scope| {
+	        connect_tcp::<Cli>(scope, &addr, url.clone())
+	    }).unwrap();
+
+	    loop_inst.run().unwrap();
+	});
+
+	handle.join();
 }
