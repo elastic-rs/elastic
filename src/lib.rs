d4a09a2754f133206430fa62db9a3c7930808020
Stephan Buys <stephan.buys@panoptix.co.za>
2016-12-28 15:57:31 +0200

    Start work on Iterator for aggregations.

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,60 @@ pub struct Response {
     timed_out: bool,
     _shards: Shards,
     hits: Hits,
-    aggregations: Option<serde_json::Value>,
+//    aggregations: Option<serde_json::Value>,
+    aggregations: Aggregations,
     status: Option<u16>
 }
+
+#[derive(Deserialize, Debug)]
+struct Aggregations(Option<serde_json::Value>);
+
+// and we'll implement IntoIterator
+impl IntoIterator for Aggregations {
+    type Item = serde_json::Value;
+    type IntoIter = AggregationIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+//        self.0.into_iter()
+        AggregationIterator::new(self)
+    }
+}
+
+struct AggregationIterator {
+    count: usize,
+    aggregations: Aggregations
+}
+
+impl AggregationIterator {
+    //FIXME: right name?
+    fn new(a: Aggregations) -> AggregationIterator {
+        AggregationIterator {
+            count: 0,
+            aggregations: a
+        }
+    }
+}
+
+impl Iterator for AggregationIterator {
+    // we will be counting with usize
+    type Item = serde_json::Value;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<serde_json::Value> {
+        // increment our count. This is why we started at zero.
+//        self.count += 1;
+
+        // check to see if we've finished counting or not.
+//        if self.count < 6 {
+//            Some(self.count)
+//        } else {
+//            None
+//        }
+        None
+    }
+
+}
+
 
 #[derive(Deserialize, Debug)]
 struct Shards {
