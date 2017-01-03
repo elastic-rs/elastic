5fb9fb5a7344a6a4e07b8b0e5dc311decfe675f6
Ashley Mannix <ashleymannix@live.com.au>
2017-01-03 18:12:53 +1000

    fix doc tests for strings

--- types/src/string/keyword/keyword.rs
+++ types/src/string/keyword/keyword.rs
@@ -13,8 +13,8 @@ use ::field::FieldType;
 /// Defining a `keyword` with a mapping:
 ///
 /// ```
-/// use elastic_types::string::mapping::DefaultKeywordMapping;
-/// use elastic_types::string::Keyword;
+/// use elastic_types::string::keyword::mapping::DefaultKeywordMapping;
+/// use elastic_types::string::keyword::Keyword;
 ///
 /// let string = Keyword::<DefaultKeywordMapping>::new("my string value");
 /// ```
@@ -35,8 +35,8 @@ impl<M> Keyword<M>
     /// Create a new `Keyword` from a `String`:
     ///
     /// ```
-    /// use elastic_types::string::mapping::DefaultKeywordMapping;
-    /// use elastic_types::string::Keyword;
+    /// use elastic_types::string::keyword::mapping::DefaultKeywordMapping;
+    /// use elastic_types::string::keyword::Keyword;
     ///
     /// let string = Keyword::<DefaultKeywordMapping>::new("my string");
     /// ```
--- types/src/string/text/text.rs
+++ types/src/string/text/text.rs
@@ -13,8 +13,8 @@ use ::field::FieldType;
 /// Defining a `text` field with a mapping:
 ///
 /// ```
-/// use elastic_types::string::mapping::DefaultTextMapping;
-/// use elastic_types::string::Text;
+/// use elastic_types::string::text::mapping::DefaultTextMapping;
+/// use elastic_types::string::text::Text;
 ///
 /// let string = Text::<DefaultTextMapping>::new("my string value");
 /// ```
@@ -35,8 +35,8 @@ impl<M> Text<M>
     /// Create a new `Text` from a `String`:
     ///
     /// ```
-    /// use elastic_types::string::mapping::DefaultTextMapping;
-    /// use elastic_types::string::Text;
+    /// use elastic_types::string::text::mapping::DefaultTextMapping;
+    /// use elastic_types::string::text::Text;
     ///
     /// let string = Text::<DefaultTextMapping>::new("my string");
     /// ```
