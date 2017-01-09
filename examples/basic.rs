a583f1445a6fbf903ce2743f8cb6833fa6c477d6
Ashley Mannix <ashleymannix@live.com.au>
2017-01-09 18:21:45 +1000

    add some docs and a sample

---
+++ examples/basic.rs
@@ -0,0 +1,41 @@
+//! A basic usage example.
+//! 
+//! NOTE: This sample expects you have a node running on `localhost:9200`.
+
+#[macro_use]
+extern crate json_str;
+extern crate elastic;
+
+use elastic::client::{self, ElasticClient};
+
+fn main() {
+	// A reqwest HTTP client
+	let client = client::Client::new().unwrap();
+
+	// Default parameters.
+	// This includes the base node url (http://localhost:9200).
+	let params = client::RequestParams::default()
+		.url_params(vec![("pretty", String::from("true"))]);
+
+	// A freeform JSON request body.
+    let body = json_str!({
+        query: {
+            query_string: {
+                query: "*"
+            }
+        }
+    });
+
+    // A search request from the body.
+    let req = client::SearchRequest::for_index("_all", body);
+
+    // Send the request and process the response.
+    let res: client::Response = client
+    	.elastic_req(&params, req).unwrap()
+    	.json().unwrap();
+
+    // Iterate through the hits in the response.
+    for hit in res.hits() {
+        println!("{:?}", hit);
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,35 @@
 //! This crate is a meta-package that makes it easy to work
 //! with the Elasticsearch REST API.
 
+extern crate reqwest;
 extern crate elastic_reqwest;
 extern crate elastic_requests;
 extern crate elastic_types;
+extern crate elastic_responses;
 
 pub mod client {
+	//! HTTP client, requests and responses.
+	//! 
+	//! This module contains the core `ElasticClient` trait, as well
+	//! as request and response types.
+
+	pub use reqwest::Client;
+
+	/// A client wrapper over [`reqwest`](https://github.com/seanmonstar/reqwest).
 	pub use elastic_reqwest::*;
+
+	/// Request types the Elasticsearch REST API.
 	pub use elastic_requests::*;
+
+	/// Response types for the Elasticsearch REST API.
+	pub use elastic_responses::*;
 }
 
 pub mod types {
+	//! Indexable documents and type mapping.
+	//! 
+	//! This module contains tools for defining Elasticsearch-compatible
+	//! document types.
+
 	pub use elastic_types::*;
 }
