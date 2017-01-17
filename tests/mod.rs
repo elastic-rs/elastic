f4704d5a1ed6d4bc1379c04590dc9ec19778de17
Ashley Mannix <ashleymannix@live.com.au>
2017-01-17 16:07:31 +1000

    add error and get doc types

--- tests/mod.rs
+++ tests/mod.rs
@@ -16,7 +16,7 @@ extern crate serde_json;
 extern crate slog_stdlog;
 extern crate slog_envlogger;
 
-use elastic_responses::Response;
+use elastic_responses::*;
 use std::fs::File;
 use std::io::Read;
 
@@ -30,7 +30,7 @@ fn load_file(p :&str) -> String {
 #[test]
 fn test_parse_hits_simple() {
     let s = load_file("tests/samples/hits_only.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     assert_eq!(deserialized.hits().into_iter().count(), 5);
 }
@@ -38,7 +38,7 @@ fn test_parse_hits_simple() {
 #[test]
 fn test_parse_simple_aggs() {
     let s = load_file("tests/samples/aggregation_simple.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     assert_eq!(deserialized.aggs().into_iter().count(), 124);
 }
@@ -46,7 +46,7 @@ fn test_parse_simple_aggs() {
 #[test]
 fn test_parse_3level_aggs() {
     let s = load_file("tests/samples/aggregation_3level.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     assert_eq!(deserialized.aggs().into_iter().count(), 201);
 }
@@ -54,7 +54,7 @@ fn test_parse_3level_aggs() {
 #[test]
 fn test_parse_3level_multichild_aggs() {
     let s = load_file("tests/samples/aggregation_3level_multichild.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     let min = "min_ack_pkts_sent";
     let avg = "avg_ack_pkts_sent";
@@ -76,7 +76,7 @@ fn test_parse_3level_multichild_aggs() {
 #[test]
 fn test_parse_3level_multistats_aggs() {
     let s = load_file("tests/samples/aggregation_3level_multistats.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     let min = "extstats_ack_pkts_sent_min";
     let avg = "stats_ack_pkts_sent_avg";
@@ -100,7 +100,7 @@ fn test_parse_3level_multistats_aggs() {
 #[test]
 fn test_parse_simple_aggs_no_empty_first_record() {
     let s = load_file("tests/samples/aggregation_simple.json");
-    let deserialized: Response = serde_json::from_str(&s).unwrap();
+    let deserialized: QueryResponse = serde_json::from_str(&s).unwrap();
 
     let s = "timechart";
     let mut first = true;
@@ -110,4 +110,70 @@ fn test_parse_simple_aggs_no_empty_first_record() {
             first = false;
         }
     }
+}
+
+#[test]
+fn test_parse_found_doc_response() {
+    let s = load_file("tests/samples/get_found.json");
+    let deserialized: GetDocResponse = serde_json::from_str(&s).unwrap();
+
+    let id = deserialized.source
+        .unwrap()
+        .as_object()
+        .and_then(|src| src.get("id"))
+        .and_then(|id| id.as_u64());
+
+    assert!(deserialized.found);
+    assert_eq!("testindex", deserialized.index);
+    assert_eq!("testtype", deserialized.ty);
+    assert_eq!(Some(8), deserialized.version);
+    assert_eq!(Some(1), id);
+}
+
+#[test]
+fn test_parse_not_found_doc_response() {
+    let s = load_file("tests/samples/get_not_found.json");
+    let deserialized: GetDocResponse = serde_json::from_str(&s).unwrap();
+
+    assert!(!deserialized.found);
+}
+
+#[test]
+fn test_parse_index_not_found_error() {
+    let s = load_file("tests/samples/error_index_not_found.json");
+    let deserialized: Error = serde_json::from_str(&s).unwrap();
+
+    let error = deserialized.error();
+
+    assert_eq!(404, deserialized.status);
+    assert_eq!(ErrorKind::IndexNotFound { index: "carrots" }, error);
+}
+
+#[test]
+fn test_parse_parsing_error() {
+    let s = load_file("tests/samples/error_parsing.json");
+    let deserialized: Error = serde_json::from_str(&s).unwrap();
+
+    let error = deserialized.error();
+
+    assert_eq!(400, deserialized.status);
+    assert_eq!(ErrorKind::Parsing { line: 2, col: 9, reason: "Unknown key for a START_OBJECT in [qry]." }, error);
+}
+
+#[test]
+fn test_parse_other_error() {
+    let s = load_file("tests/samples/error_other.json");
+    let deserialized: Error = serde_json::from_str(&s).unwrap();
+
+    let error = deserialized.error();
+
+    let reason = match error {
+        ErrorKind::Other(err) => err.as_object()
+                                    .and_then(|err| err.get("reason"))
+                                    .and_then(|reason| reason.as_str()),
+        _ => None
+    };
+
+    assert_eq!(500, deserialized.status);
+    assert_eq!(Some("An error that we don't know about."), reason);
 }
