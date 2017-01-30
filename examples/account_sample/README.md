13e2bb3514834cebf277462dfb346f17d74813cf
Ashley Mannix <ashleymannix@live.com.au>
2017-01-30 12:28:50 +1000

    add test for index body

--- examples/account_sample/README.md
+++ examples/account_sample/README.md
@@ -1,3 +1,5 @@
 # Account Sample
 
 This is a more complete sample using `elastic`, using the hypothetical [bank account sample data](https://www.elastic.co/guide/en/elasticsearch/reference/current/_exploring_your_data.html).
+
+The code contains inline tests to show you what the result of some operations like document mapping should be.
--- examples/account_sample/src/model/index.rs
+++ examples/account_sample/src/model/index.rs
@@ -1,4 +1,7 @@
 use elastic::client::requests::IndicesCreateRequest;
+use elastic::types::prelude::{Document, DocumentType, FieldType};
+use serde_json;
+use super::account::Account;
 
 pub fn name() -> &'static str {
     INDEX
@@ -11,16 +14,24 @@ pub fn request() -> IndicesCreateRequest<'static> {
 const INDEX: &'static str = "bank-sample";
 
 fn bank_index() -> String {
-    let get_index = json_fn!(|filters, analysers| {
+    let account_name = format!("\"{}\"", Account::name());
+    let account_mapping = serde_json::to_string(&Document::from(Account::mapping())).expect("get Account mapping");
+    let filters = bank_filters();
+    let analysers = bank_analysers();
+
+    let get_index = json_fn!(|filters, analysers, account_name, account_mapping| {
        "settings" : {
           "analysis" : {
-             "filter" : $filters,
-             "analyzer" : $analysers
+                "filter" : $filters,
+                "analyzer" : $analysers
+          },
+          "mappings": {
+            $account_name: $account_mapping
           }
        }
     });
-
-    get_index(&bank_filters(), &bank_analysers())
+    
+    get_index(&filters, &analysers, &account_name, &account_mapping)
 }
 
 fn bank_filters() -> String {
@@ -49,4 +60,97 @@ fn bank_analysers() -> String {
             ]
         }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+    use elastic::client::requests::RawBody;
+    use super::*;
+
+    #[test]
+    fn request_url() {
+        let req = request();
+
+        assert_eq!("/bank-sample", req.url.as_ref());
+    }
+
+    #[test]
+    fn request_body() {
+        let req = request();
+
+        let body = req.body.into_raw();
+        let body = str::from_utf8(&body).unwrap();
+
+        let expected = json_str!({
+            "settings":{
+                "analysis":{
+                    "filter":{
+                        "email":{
+                            "type":"pattern_capture",
+                            "preserve_original":1,
+                            "patterns":[
+                                "([^@]+)",
+                                "(\\p{L}+)",
+                                "(\\d+)",
+                                "@(.+)"
+                            ]
+                        }
+                    },
+                    "analyzer":{
+                        "email":{
+                            "tokenizer":"uax_url_email",
+                            "filter":[
+                                "email",
+                                "lowercase",
+                                "unique"
+                            ]
+                        }
+                    }
+                },
+                "mappings":{
+                    "account":{
+                        "properties":{
+                            "account_number":{
+                                "type":"integer"
+                            },
+                            "balance":{
+                                "type":"integer"
+                            },
+                            "firstname":{
+                                "type":"keyword"
+                            },
+                            "lastname":{
+                                "type":"keyword"
+                            },
+                            "age":{
+                                "type":"byte"
+                            },
+                            "gender":{
+                                "type":"keyword"
+                            },
+                            "address":{
+                                "type":"text"
+                            },
+                            "employer":{
+                                "type":"keyword"
+                            },
+                            "email":{
+                                "type":"text",
+                                "analyzer":"email"
+                            },
+                            "city":{
+                                "type":"keyword"
+                            },
+                            "state":{
+                                "type":"keyword"
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        assert_eq!(expected, body);
+    }
 }
