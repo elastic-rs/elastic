dde503b91ae46d4e7522f1590514ced9196708c1
Ashley Mannix <ashleymannix@live.com.au>
2017-04-08 20:43:15 +1000

    add an error type

--- src/bulk.rs
+++ src/bulk.rs
@@ -1,8 +1,28 @@
+use serde_json::{Map, Value};
 use parse::MaybeOkResponse;
 use common::Shards;
 use super::{HttpResponse, FromResponse, ApiResult};
 
 use std::io::Read;
+
+type BulkError = Map<String, Value>;
+
+// TODO: The `success` functions might be a bit pessimistic
+
+#[derive(Deserialize, Debug)]
+pub struct BulkResponse {
+    pub took: u64,
+    pub errors: bool,
+    pub items: Vec<BulkItem>,
+}
+
+impl BulkResponse {
+    pub fn success(&self) -> bool {
+        let any_item_errors = self.items.iter().any(|item| !item.success());
+
+        !self.errors && !any_item_errors
+    }
+}
 
 #[derive(Deserialize, Debug)]
 pub enum BulkItem {
@@ -36,13 +56,14 @@ pub struct BulkIndexItem {
     #[serde(rename = "_shards")]
     pub shards: Shards,
     pub status: u16,
+    pub error: Option<BulkError>,
     pub created: bool,
 }
 
 impl BulkIndexItem {
     fn success(&self) -> bool {
-        match self.status {
-            200...299 => true,
+        match (&self.error, self.status) {
+            (&None, 200...299) => true,
             _ => false,
         }
     }
@@ -61,13 +82,14 @@ pub struct BulkCreateItem {
     #[serde(rename = "_shards")]
     pub shards: Shards,
     pub status: u16,
+    pub error: Option<BulkError>,
     pub created: bool,
 }
 
 impl BulkCreateItem {
     pub fn success(&self) -> bool {
-        match self.status {
-            200...299 => true,
+        match (&self.error, self.status) {
+            (&None, 200...299) => true,
             _ => false,
         }
     }
@@ -86,12 +108,13 @@ pub struct BulkUpdateItem {
     #[serde(rename = "_shards")]
     pub shards: Shards,
     pub status: u16,
+    pub error: Option<BulkError>,
 }
 
 impl BulkUpdateItem {
     pub fn success(&self) -> bool {
-        match self.status {
-            200...299 => true,
+        match (&self.error, self.status) {
+            (&None, 200...299) => true,
             _ => false,
         }
     }
@@ -110,13 +133,14 @@ pub struct BulkDeleteItem {
     #[serde(rename = "_shards")]
     pub shards: Shards,
     pub status: u16,
+    pub error: Option<BulkError>,
     pub found: bool,
 }
 
 impl BulkDeleteItem {
     pub fn success(&self) -> bool {
-        match self.status {
-            200...299 | 404 => true,
+        match (&self.error, self.status) {
+            (&None, 200...299) | (&None, 404) => true,
             _ => false,
         }
     }
