883751be2ccab75a17f80a0af7d3482b619d69f9
Ashley Mannix <ashleymannix@live.com.au>
2017-04-11 19:47:53 +1000

    add a get sample

---
+++ elastic/examples/get.rs
@@ -0,0 +1,37 @@
+//! Also see the `typed` sample for a more complete implementation.
+
+extern crate serde_json;
+extern crate elastic;
+
+use serde_json::Value;
+use elastic::error::*;
+use elastic::prelude::*;
+
+fn main() {
+    // A reqwest HTTP client and default parameters.
+    // The `params` includes the base node url (http://localhost:9200).
+    let client = Client::new(RequestParams::default()).unwrap();
+
+    let req = GetRequest::for_index_ty_id("typed_sample_index", "mytype", "1");
+
+    let get_res = client.request(req)
+                        .send()
+                        .and_then(|res| res.response::<GetResponse<Value>>());
+
+    match get_res {
+        // The doc was found: no need to index
+        Ok(GetResponse { source: Some(doc), .. }) => {
+            println!("document found: {:?}", doc);
+        },
+        // The index exists, but the doc wasn't found: map and index
+        Ok(_) => {
+            println!("document not found");
+        },
+        // No index: create it, then map and index
+        Err(Error(ErrorKind::Api(ApiError::IndexNotFound { .. }), _)) => {
+            println!("index not found");
+        },
+        // Something went wrong: panic
+        Err(e) => panic!(e)
+    }
+}
