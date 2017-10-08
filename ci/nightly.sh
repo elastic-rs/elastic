5ad9b377c8af84a27503c62a62590d88fabaf6ea
Ashley Mannix <ashleymannix@live.com.au>
2017-10-08 20:30:55 +1000

    update build script

--- ci/nightly.sh
+++ ci/nightly.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-set -o errexit -o nounset
+set -o errexit -o nounset -o xtrace
 
 if [ "$KIND" == "build" ]; then
     cargo test --verbose --all
 
     BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
-    if [ "$BRANCH" == "master" ]; then
-        echo "uploading crate docs"
+    echo "uploading crate docs"
 
-        cargo doc --all
+    cargo doc --all
 
-        REV=$(git rev-parse --short HEAD)
-        cd target/doc
-        rm -r .git &>/dev/null
-        git init
-        git remote add upstream "https://$GH_TOKEN@github.com/elastic-rs/elastic.git"
-        git config user.name "elastic-rs"
-        git config user.email "travis@elastic.rs"
-        git add -A .
-        git commit -qm "Build docs at ${TRAVIS_REPO_SLUG}@${REV}"
+    REV=$(git rev-parse --short HEAD)
+    cd target/doc
+    rm -r .git &>/dev/null
+    git init
+    git remote add upstream "https://$GH_TOKEN@github.com/elastic-rs/elastic.git"
+    git config user.name "elastic-rs"
+    git config user.email "travis@elastic.rs"
+    git add -A .
+    git commit -qm "Build docs at ${TRAVIS_REPO_SLUG}@${REV}"
 
-        echo "Pushing gh-pages to GitHub"
-        git push -q upstream HEAD:refs/heads/gh-pages --force
-    fi
+    echo "Pushing gh-pages to GitHub"
+    git push -q upstream HEAD:refs/heads/gh-pages --force
 elif [ "$KIND" == "bench" ]; then
     cargo bench --verbose --all
 
