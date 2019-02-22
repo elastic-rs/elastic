60e643ac4008169d087d5a121ed21414b37005ed
Ashley Mannix <ashleymannix@live.com.au>
2019-02-22 14:41:05 +1000

    fix invalid bulk index format

--- tests/run/src/document/mod.rs
+++ tests/run/src/document/mod.rs
@@ -7,6 +7,7 @@ mod compile_test;
 
 mod delete;
 mod simple_index_get;
+mod simple_mapping;
 mod update_no_index;
 mod update_with_doc;
 mod update_with_inline_script;
@@ -15,6 +16,7 @@ mod update_with_script;
 pub fn tests() -> Vec<Test> {
     vec![
         Box::new(|client| test(client, simple_index_get::SimpleIndexGet)),
+        Box::new(|client| test(client, simple_mapping::SimpleMapping)),
         Box::new(|client| test(client, update_with_doc::UpdateWithDoc)),
         Box::new(|client| test(client, update_with_script::UpdateWithScript)),
         Box::new(|client| test(client, update_with_inline_script::UpdateWithInlineScript)),
