29e34cb621a6c3053a30711d65d79cecdde3d57d
William Myers <mwilliammyers@gmail.com>
2019-08-26 13:20:53 -0600

    Add support for _seq_no & _primary_term in responses

--- src/elastic/src/client/responses/document_get.rs
+++ src/elastic/src/client/responses/document_get.rs
@@ -32,6 +32,10 @@ pub struct GetResponse<T> {
     id: String,
     #[serde(rename = "_version")]
     version: Option<u32>,
+    #[serde(rename = "_seq_no")]
+    sequence_number: Option<u32>,
+    #[serde(rename = "_primary_term")]
+    primary_term: Option<u32>,
     found: bool,
     #[serde(rename = "_source")]
     source: Option<T>,
@@ -73,6 +77,24 @@ impl<T> GetResponse<T> {
     /** The version of the document. */
     pub fn version(&self) -> Option<u32> {
         self.version.clone()
+    }
+
+    /**
+     * The [sequence number] of the document.
+     *
+     * [sequence number]: https://www.elastic.co/guide/en/elasticsearch/reference/current/optimistic-concurrency-control.html
+     */
+    pub fn sequence_number(&self) -> Option<u32> {
+        self.sequence_number.clone()
+    }
+
+    /**
+     * The [primary term] of the document.
+     *
+     * [primary term]: https://www.elastic.co/guide/en/elasticsearch/reference/current/optimistic-concurrency-control.html
+     */
+    pub fn primary_term(&self) -> Option<u32> {
+        self.primary_term.clone()
     }
 }
 
